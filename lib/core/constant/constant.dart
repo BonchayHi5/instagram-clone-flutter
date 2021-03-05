396ace4adb72828bd7b0f7fd1e3299f386e67042
Bonchay Ouk <bonchayouk@gmail.com>
2021-03-05 23:57:01 +0700

    add scrollbar

--- lib/core/constant/constant.dart
+++ lib/core/constant/constant.dart
@@ -20,4 +20,4 @@ const double kDefaultPadding = 12.0;
 const double kDefaultIconSize = 25.0;
 
 const String igProfileUrl =
-    'https://instagram.fpnh10-1.fna.fbcdn.net/v/t51.2885-19/s150x150/103005221_647787632763113_2762888834541907464_n.jpg?_nc_ht=instagram.fpnh10-1.fna.fbcdn.net&_nc_ohc=tQcxvX2TcH8AX9WmmLB&tp=1&oh=979959230d01188da09330619ebc6099&oe=603D4E9C';
+    'https://images.pexels.com/photos/1040881/pexels-photo-1040881.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260';
